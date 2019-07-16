aabf037ceb7431daef777f2893623291caa3e356
yogesh.samant <yogesh.samant@gmail.com>
2019-07-16 16:30:30 -0400

    Refactored Chain of responsiblity by delegating a common print feature to the super class

--- src/test/java/patterns/behavioral/chainofresponsibility/ALoggerShould.java
+++ src/test/java/patterns/behavioral/chainofresponsibility/ALoggerShould.java
@@ -14,26 +14,24 @@ public class ALoggerShould {
 	final String MESSAGE = "This is a message";
 	Logger logger;
 	Console console;
-	
+	InOrder inorder;
 	
 	@Before
 	public void setup(){
 		console = mock(Console.class);
 		logger = Logger.buildLogger(console);
-		
+		inorder = inOrder(console);
 	}
 
 	@Test
 	public void print_debug_log_only_when_specified_by_debug_logging_level() {
 		logger.print(Logger.DEBUG, MESSAGE);
-		InOrder inorder = inOrder(console);
 		inorder.verify(console).printLine("DEBUG:" + MESSAGE);
 	}
 	
 	@Test
 	public void print_error_and_debug_log_only_when_specified_by_error_logging_level() {
 		logger.print(Logger.ERROR, MESSAGE);
-		InOrder inorder = inOrder(console);
 		inorder.verify(console).printLine("DEBUG:" + MESSAGE);
 		inorder.verify(console).printLine("ERROR:" + MESSAGE);
 	}
@@ -41,7 +39,6 @@ public class ALoggerShould {
 	@Test
 	public void print_all_error_and_debug_log_only_when_specified_by_all_logging_level() {
 		logger.print(Logger.ALL, MESSAGE);
-		InOrder inorder = inOrder(console);
 		inorder.verify(console).printLine("DEBUG:" + MESSAGE);
 		inorder.verify(console).printLine("ERROR:" + MESSAGE);
 		inorder.verify(console).printLine("ALL:" + MESSAGE);
