1c53dc4d1af9070152c8143adb4dbf7a03716953
yogesh.samant <yogesh.samant@gmail.com>
2019-07-30 16:33:34 -0400

    Intepreter pattern implementing the infix to postfix expression converter

---
+++ src/test/java/patterns/behavioral/observer/PostFixPatternShould.java
@@ -0,0 +1,24 @@
+package patterns.behavioral.observer;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
+import patterns.behavioral.intepreter.InterpretPattern;
+
+public class PostFixPatternShould {
+
+	@Test
+	public void test() {
+		InterpretPattern postFix = new PostFixPattern();
+		assertThat(postFix.convert("2+3"), equalTo("23+"));
+		
+		assertThat(postFix.convert("2+3*6"), equalTo("236*+"));
+		
+		assertThat(postFix.convert("2+3*6-4"), equalTo("2364-*+"));
+		
+	}
+
+}
