f3cfe0f2875f28673406bea93ad3590607324f28
YogeSam <39127957+YogeSam@users.noreply.github.com>
2019-08-30 13:51:10 -0400

    Update DbConnection.java

--- src/main/java/dbConnection/DbConnection.java
+++ src/main/java/dbConnection/DbConnection.java
@@ -8,7 +8,7 @@ public class DbConnection {
 
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");  
-		Connection con= DriverManager.getConnection( "jdbc:mysql://localhost:4928/test","ecwDbUser","Pwd4db@114");
+		Connection con= DriverManager.getConnection( "jdbc:mysql://localhost:4928/test","root","test");
 		return con;
 	}
 
